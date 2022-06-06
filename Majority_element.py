80fc4a537cba29732662fbedcf28a1d07518fd99
divyanshpandey03396 <divyanshpandey03396@gmail.com>
2022-06-06 22:46:31 +0530

    +3

---
+++ Majority_element.py
@@ -0,0 +1,49 @@
+#User function template for Python 3
+
+class Solution:
+    def majorityElement(self, A, N):
+        #Your code here
+         ele,count=0,0
+         for num in A:
+           if count==0:
+               ele=num
+           if ele==num:
+               count+=1
+           else:
+               count-=1
+         count=0           
+         for i in range(N):
+            if A[i]==ele:
+                count+=1
+         if count>N/2:
+             return ele
+         return -1
+         
+         
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+from sys import stdin
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+
+            A=[int(x) for x in input().strip().split()]
+            
+            
+            obj = Solution()
+            print(obj.majorityElement(A,N))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
